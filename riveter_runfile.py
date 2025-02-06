acb14d2f7e2991b91cc2e8dab2a26072918e282b
Julia Neugarten <70568853+julianeugarten@users.noreply.github.com>
2025-02-06 11:06:03 +0100

    updated

--- riveter_runfile.py
+++ riveter_runfile.py
@@ -1,4 +1,4 @@
-# conda activate riveterEnv
+# conda activate riveterEnv (create your own virtual env if desired)
 
 #import libraries
 import os
@@ -7,20 +7,20 @@ from riveter import Riveter
 import pandas as pd
 
 #load the data
-hadper_df = pd.read_csv('hadper_short_stories.csv', on_bad_lines='skip')
-work_ids = hadper_df["work_id"].to_list()
-stories = hadper_df["story"].to_list()
+hadper_df = pd.read_csv('dataset.csv', on_bad_lines='skip') # change filename to your own
+work_ids = hadper_df["work_id"].to_list() # your column of work-ids may have a different name
+stories = hadper_df["story"].to_list() # your column of texts may have a different name
 
 #load lexicon and train model
 riveter = Riveter()
-riveter.load_sap_lexicon('agency') # you can change 'power' to 'agency' to get another dimension
+riveter.load_sap_lexicon('power') # you can change 'power' to 'agency' to get another dimension
 riveter.train(stories,
               work_ids)
 
 
-# save the model
-riveter.save('hadper_CCLS_agency.pkl')
+# save the model under a chosen filename
+riveter.save('yourname_power.pkl')
 
 # load saved results into riveter this way:
-# riveter = Riveter(filename = "hadper_power.pkl")
+# riveter = Riveter(filename = "yourname_power.pkl")
 
